87bdc82428e6fe36eff85ce542f8894cde3d2704
IMAGEGME-0524\Teilnehmer <matthias.hofherr@web.de>
2025-09-18 09:21:25 +0200

    Inventory Test Crowbar

---
+++ Infested/Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+
+    // Reference to the data associated with this item (e.g., name, icon, type)
+    public ItemData data;
+}
---
+++ Infested/Assets/Scripts/Inventory/ItemData.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+// Create a new item asset from the Unity editor via the "Assets > Create > Inventory > Item" menu
+[CreateAssetMenu(fileName = "NewItem", menuName = "Inventory/Item")]
+public class ItemData : ScriptableObject
+{
+
+    // The display name of the item
+    public string itemName;
+
+    // A unique identifier for the item
+    public int itemID;
+
+    // Optional: an icon for the item to be used in the UI
+    // public Sprite icon;
+}
